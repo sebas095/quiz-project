b264d08c3f8a4aec4cc6bc02600eaebc79c6607a
Sebastian Duque <sebas_tian_95@hotmail.com>
2015-07-30 23:45:39 -0500

    Validacion de entradas

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -23,14 +23,14 @@ exports.index = function(req, res){
 		search += '%';
 		//console.log(search);
 		models.Quiz.findAll({where:["pregunta LIKE ?", search], order:"question"}).then(function(quizes){
-			res.render('quizes/index.ejs', { quizes: quizes });
+			res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
 			}
 		).catch(function(error){ next(error); });
 	}
 
 	else{
 		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index.ejs', { quizes: quizes });
+			res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
 			}
 		).catch(function(error){ next(error); });
 	}
@@ -42,22 +42,30 @@ exports.new = function(req, res){
 		{ pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
 
-	res.render('quizes/new', { quiz: quiz });
+	res.render('quizes/new', { quiz: quiz, errors: [] });
 };
 
 // POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build( req.body.quiz );
 
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({ fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) // Redireccion HTPP (URL relativo) lista de preguntas
+	quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/new', { quiz: quiz, errors: err.errors});
+		}
+		else{
+			// guarda en DB los campos pregunta y respuesta de quiz
+			quiz.save({ fields: ["pregunta", "respuesta"]}).then(function(){
+				res.redirect('/quizes');
+			}) // Redireccion HTPP (URL relativo) lista de preguntas
+		}
+	});
+	
 };
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show',{ quiz: req.quiz});
+	res.render('quizes/show',{ quiz: req.quiz, errors: [] });
 };
 
 // GET /quizes/:id/answer
@@ -66,12 +74,13 @@ exports.answer = function(req, res){
 	if(req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado });
+	res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado, errors: [] });
 };
 
 exports.authors = function(req, res){
 	res.render('quizes/author',{
-		autores: '<img src="/images/perfil.jpg" width="120" height="150" alt="Photo Perfil"/><br><h3>Sebastian Duque Restrepo</h3>'
+		autores: '<img src="/images/perfil.jpg" width="120" height="150" alt="Photo Perfil"/><br><h3>Sebastian Duque Restrepo</h3>',
+		errors: []
 	});
 };
 
