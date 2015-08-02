d448edd10bffdf2cb471e26ae55a1d92e2104a43
Sebastian Duque <sebas_tian_95@hotmail.com>
2015-08-02 16:34:53 -0500

    Editar pregunta

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -17,23 +17,21 @@ exports.load = function(req, res, next, quizId){
 
 // GET /quizes/
 exports.index = function(req, res){
-	if(req.query.search){
-		var search = '%';
-		search += (req.query.search).replace(/ /g,'%');
-		search += '%';
-		//console.log(search);
-		models.Quiz.findAll({where:["pregunta LIKE ?", search], order:"question"}).then(function(quizes){
-			res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
-			}
-		).catch(function(error){ next(error); });
+	var busqueda = req.query.search;
+	if(busqueda === undefined){
+		models.Quiz.findAll().then(function (quizes){
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+        		}
+     		).catch(function(error) { next(error);})
 	}
-
 	else{
-		models.Quiz.findAll().then(function(quizes){
+		busqueda = '%'+busqueda.replace(/ /g,'%')+'%';
+		models.Quiz.findAll({where:["pregunta LIKE ?", busqueda], order:"question"}).then(function(quizes){
 			res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
 			}
 		).catch(function(error){ next(error); });
 	}
+		
 };
 
 // GET /quizes/new
@@ -61,6 +59,31 @@ exports.create = function(req, res){
 		}
 	});
 	
+};
+
+// GET/quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz; // autoload de instancia quiz
+
+	res.render('quizes/edit', { quiz:quiz, errors: [] });
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/edit', { quiz:req.quiz, errors: err.errors});
+		}
+		else{
+			req.quiz.save({ fields: ["pregunta","respuesta"]})// guarda en DB los campos pregunta y respuesta de quiz
+			.then(function(){
+				res.redirect('/quizes'); // Redireccion HTPP (URL relativo) lista de preguntas
+			});
+		}
+	});
 };
 
 // GET /quizes/:id
