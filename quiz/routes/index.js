fe8b5216c83d29fe0f989d4525cd44252ae99881
Sebastian Duque <sebas_tian_95@hotmail.com>
2015-08-10 00:36:16 -0500

    Autenticacion y sesion

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId',quizController.load); // autoload :quizId
+
+// Definicion de rutas de sesion
+router.get('/login',	sessionController.new); // formulario login
+router.post('/login',	sessionController.create); // crear sesion
+router.get('/logout',	sessionController.destroy); // destruir sesion
 
 // Definicion de rutas de /quizes
 router.get('/quizes',			   			quizController.index);
@@ -23,6 +29,7 @@ router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
 router.put('/quizes/:quizId(\\d+)', 		quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
 
+// Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
